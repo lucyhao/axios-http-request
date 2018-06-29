5c3130f13d8eef7b22cb53d2c360af642153abbe
Hao Li <haol@seagroup.com>
2018-06-29 10:38:54 +0800

    add response handle

--- index.js
+++ index.js
@@ -1,57 +1,70 @@
 var axios = require('axios');
-var promiseArr = {};
+var cancelObj = {};
 
 function request(options) {
   return new Promise((resolve, reject) => {
-   var cancel;
+    var cancel;
 
-   var httpServer = axios.create({
-     responseType: 'json',
-     method: options.method,
-     baseURL : options.url,
-     headers: {
-       'Content-Type': 'application/json',
-     },
-     transformRequest: [function (data) {
-       return JSON.stringify(data);
-     }],
-     cancelToken: new axios.CancelToken(function (c) {
-       cancel = c  // record the cancel function
-     })
-   })
+    var httpServer = axios.create({
+      responseType: 'json',
+      method: options.method,
+      baseURL : options.url,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      transformRequest: [function (data) {
+        return JSON.stringify(data);
+      }],
+      cancelToken: new axios.CancelToken(function (c) {
+        cancel = c  // record the cancel function
+      })
+    })
 
-   var common = httpServer.defaults.headers.common;
-   httpServer.defaults.headers.common = Object.assign(common, options.headers);
-   // every request will do this
-   httpServer.interceptors.request.use(function (config) {
-     var cancelMultiple = options.cancelMultiple ? options.cancelMultiple : false;
-     //if need cancelMultiple
-     if(cancelMultiple){
-       if (promiseArr[config.baseURL]) {//
+    var common = httpServer.defaults.headers.common;
+    httpServer.defaults.headers.common = Object.assign(common, options.headers);
+    // every request will do this
+    httpServer.interceptors.request.use(function (config) {
+      var cancelMultiple = options.cancelMultiple ? options.cancelMultiple : false;
+      //if need cancelMultiple
+      if(cancelMultiple){
+        if (cancelObj[config.baseURL]) {//
 
-         promiseArr[config.baseURL] = cancel;
-         promiseArr[config.baseURL]('request cancelled');
+          cancelObj[config.baseURL] = cancel;
+          cancelObj[config.baseURL]('request cancelled');
+
+        } else {
+          cancelObj[config.baseURL] = cancel;
+        }
+      }
+      config.data = options.data ? options.data : {};
+      return config;
+    
+    }, function (error) {
+    
+      return Promise.reject(error);
+    })
+
+    httpServer.interceptors.response.use(function(response){
+      
+      cancelObj[response.config.baseURL] = null;
+      return response;
+      
+    }, function(error){
+      if (error.response) {
+        return Promise.reject(error);
+      } else if (error.request) {
+        return Promise.reject({data: 'no response'});
+      } else {
+        return Promise.reject({data: error.message});
+      }
+      
+    });
   
-       } else {
-         promiseArr[config.baseURL] = cancel;
-       }
-     }
-     config.data = options.data ? options.data : {};
-     return config;
-    
-   }, function (error) {
-    
-     return Promise.reject(error);
-   })
-
-   httpServer().then(res => {
-     promiseArr = {};
-     resolve(res.data);
-     //return false
-   }).catch(error => {
-    
-     reject(error)
-   })
+    httpServer().then(res => {
+      resolve(res.data);
+    }).catch(err => {
+      reject(err)
+    })
 
  })
 }
